9f62b431ea9304ca72a0b84f00885750ea3361da
Felipe Cunha <felipe.cunha@afya.com.br>
2020-10-01 15:15:11 -0300

    creating middlewares and connecting db with knex

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const app = require("express")();
+const db = require("./config/db");
+const consign = require("consign");
+
+consign().then("./config/middleware.js").into(app);
 
 app.get("/", (req, res) => {
   res.status(200).send("Backend");
 });
+
+app.db = db;
 
 app.listen(3000, () => {
   console.log("Backend starting...");
