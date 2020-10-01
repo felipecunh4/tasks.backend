9b7098446693e7d3791c0c0903f3744632b47ecb
Felipe Cunha <felipe.cunha@afya.com.br>
2020-10-01 15:37:18 -0300

    saving users

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const app = require("express")();
 const db = require("./config/db");
 const consign = require("consign");
 
-consign().then("./config/middleware.js").into(app);
+consign()
+  .then("./config/middleware.js")
+  .then("./api")
+  .then("./config/routes.js")
+  .into(app);
 
 app.get("/", (req, res) => {
   res.status(200).send("Backend");
